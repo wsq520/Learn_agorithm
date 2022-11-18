d5cc4a10c3c3e3bebe9cc5e8413e0e14c6784e3a
Coderwsq <9699507+wsq__666@user.noreply.gitee.com>
2022-11-18 22:18:56 +0800

    237删除链表节点

---
+++ 链表/237.删除链表节点.ts
@@ -0,0 +1,17 @@
+// https://leetcode.cn/problems/delete-node-in-a-linked-list/solutions/?languageTags=typescript
+
+class ListNode {
+  val: number
+  next: ListNode | null
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = (val === undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
+  }
+}
+
+function deleteNode(node: ListNode | null): void {
+  node!.val = node!.next!.val
+  node!.next = node!.next!.next
+}
+
+export { }
