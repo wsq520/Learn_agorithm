418fa437eac9f40445797e969c4d766c9be6ea43
Coderwsq <9699507+wsq__666@user.noreply.gitee.com>
2023-01-07 13:31:44 +0800

    滑动窗口-存在重复元素II

---
+++ 滑动窗口/219.存在重复元素II.ts
@@ -0,0 +1,17 @@
+// https://leetcode.cn/problems/contains-duplicate-ii/description/?languageTags=javascript
+
+function containsNearbyDuplicate(nums: number[], k: number): boolean {
+  const set = new Set()
+  for (let i = 0; i < nums.length; i++) {
+    if (set.has(nums[i])) {
+      return true
+    }
+    set.add(nums[i])
+    if (set.size > k) {
+      set.delete(nums[i - k])
+    }
+  }
+  return false
+};
+
+export { }
